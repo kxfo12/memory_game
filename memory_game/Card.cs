ddc70eb57483138f930006e4cbf7d0922adced59
kxfo <147588035+kxfo12@users.noreply.github.com>
2024-11-29 11:41:07 +0100

    Add files via upload

---
+++ memory_game/Card.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+
+namespace memory_game
+{
+    internal class Card
+    {
+        public ImageSource source;
+        public bool onReverse;
+
+        public Card(ImageSource source)
+        {
+            this.source = source;
+            this.onReverse = true;
+        }
+        public ImageSource Reverse()
+        {
+            if (onReverse)
+            {
+                return source;
+            }
+            else
+            {
+                ImageSource odwrot = new BitmapImage(new Uri("carlotta.png", UriKind.Relative));
+                return odwrot;
+            }
+        }
+    }
+}
---
+++ memory_game/MainWindow.xaml.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace memory_game
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        List<Card> characters = new List<Card>();
+        Card[] randomCharacters = new Card[12];
+        int counter = 0;
+        int lastIdCLicked = -1;
+        public MainWindow()
+        {
+            InitializeComponent();
+
+        }
+        public void shuffle()
+        {
+            int i = 0;
+            while (i < 12)
+            {
+                int rand = new Random().Next(0, 11);
+                if (randomCharacters[rand] == null)
+                {
+                    randomCharacters[rand] = characters[i];
+                    i++;
+                }
+            }
+        }
+
+        private void choose(object sender, RoutedEventArgs e, int id)
+        {
+            
+            if (lastIdCLicked != id)
+            {
+                
+                lastIdCLicked = id;
+                Image button = (Image)FindName("image" + id);
+                button.Source = randomCharacters[id].Reverse();
+            }
+        }
+    }
+}
